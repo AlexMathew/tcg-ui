5374538f33f21867c1d9eaeb1fa0d0da51394410
Alex Mathew <alexmathew003@gmail.com>
2014-02-16 16:27:19 +0530

    Add basic dropdown menu framework

--- setup_cardsets.py
+++ setup_cardsets.py
@@ -11,13 +11,13 @@ class CardOperations(object):
 		self.in_ctrl = new_ctrl
 
 	def toss(self):
-		self.in_ctrl = random.randrange(1, 3)
+		self.in_ctrl = random.randrange(0, 2)
 
 	def toss_result(self):
 		self.toss()
 		html_begin_text = open("templates/original_begin.html").read()
 		new_html_begin_text = html_begin_text \
-							  .replace("***tosswinner***", "Player " + str(self.in_ctrl) \
+							  .replace("***tosswinner***", "Player " + str(self.in_ctrl + 1) \
 							  			+ " won the toss, so he will get to start") 
 		
 		with open("templates/begin.html", "w") as f:
@@ -33,15 +33,18 @@ class CardOperations(object):
 		p2 = self.ps.PlayerSet2[0]
 		self.ps.PlayerSet2.rotate(-1)
 
+		if self.in_ctrl == 0:
+			p = p1
+		else:
+			p = p2
+
 		html_text = open("templates/original_game.html").read()
 		new_html_text = html_text \
-						.replace("***inctrl***", str(self.in_ctrl)) \
-						.replace("***player1name***", str(p1.name)) \
-						.replace("***player1img***", str(p1.img_url)) \
-						.replace("***player2name***", str(p2.name)) \
-						.replace("***player2img***", str(p2.img_url))
+						.replace("***inctrl***", str(self.in_ctrl + 1)) \
+						.replace("***playername***", str(p.name)) \
+						.replace("***playerimg***", str(p.img_url))
 
 		with open("templates/game.html", "w") as f:
 			f.write(new_html_text) 
 
-		self.modify_ctrl(random.randrange(1, 3))
+		self.modify_ctrl(random.randrange(0, 2))
